a180d7045415b9a3147f54a18cbcb87c81518dfc
FerdousArpon <167005918+FerdousArpon@users.noreply.github.com>
2024-04-30 13:44:33 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
+let count = 0
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+function save() {
+    let saveAs = count + ' - '
+    saveEl.textContent += saveAs
+    countEl.textContent = 0
+    count = 0
+}
